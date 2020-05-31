6b82aa9c1499c1fb397e7690c642d618ac5a2653
Mário Santos <super.mario.santos.sousa@gmail.com>
2020-05-31 09:43:46 +0100

    Add bootstrap template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CordelDetailComponent } from './cordels/cordel-detail/cordel-detail.component';
 import { CordelListComponent } from './cordels/cordel-list/cordel-list.component';
+import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
   {path:'cordel/:id', component: CordelDetailComponent},
-  {path:'', component: CordelListComponent}
+  {path:'', component: AppComponent}
 ];
 
 @NgModule({
