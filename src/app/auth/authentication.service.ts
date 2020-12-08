2b5efd13809b9a491bdab87d7331ea8c5d1e1e2e
Mario Sousa <m_santos_sousa@hotmail.com>
2020-12-08 19:19:15 +0000

    Add login and logout menus

--- src/app/auth/authentication.service.ts
+++ src/app/auth/authentication.service.ts
@@ -10,6 +10,7 @@ import * as moment from 'moment';
 
 import decode from 'jwt-decode';
 import { TokenPayload } from './token-payload';
+import { Router } from '@angular/router';
 
 const TOKEN = "token";
 const EXPIRES_AT = "expires_at";
@@ -21,7 +22,7 @@ export class AuthenticationService {
   
   private loginUrl = `${environment.API_URL}/auth`;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private router: Router) { }
 
   login(username: string, password: string) {
     return this.httpClient.post(this.loginUrl, { username, password })
@@ -38,6 +39,11 @@ export class AuthenticationService {
   }
 
   logout() {
+    this.cleanLocalStorage();
+    this.router.navigateByUrl("/");
+  }
+
+  private cleanLocalStorage() {
     localStorage.removeItem(TOKEN);
     localStorage.removeItem(EXPIRES_AT);
   }
